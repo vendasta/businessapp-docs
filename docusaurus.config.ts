2497ce28497409344dd612d21a7360ccc933ecdf
Jesse Redl <jredl@vendasta.com>
2024-06-17 08:13:00 -0600

    remove references to docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/vendasta/businessapp',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -93,7 +93,7 @@ const config: Config = {
           label: 'Overview',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/vendasta/businessapp',
           label: 'GitHub',
           position: 'right',
         },
