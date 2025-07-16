69f90bc8151163d5a4f647b5ee289db8b8fe6c8a
Jon Neher <jneher@vendasta.com>
2025-07-16 13:37:07 -0600

    added Favicon, social share image, title description for SEO

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
         //... other Algolia params
       },
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/businessapp-docs-social-card.png',
     navbar: {
       title: 'Business App',
       logo: {
