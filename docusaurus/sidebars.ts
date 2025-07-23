7be47d84059da456abe0d12270e3eaf8caca54dc
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-07-23 14:04:32 -0600

    structure and basic details of article

--- docusaurus/sidebars.ts
+++ docusaurus/sidebars.ts
@@ -109,6 +109,7 @@ const docsSidebar: SidebarsConfig['docs'] = [
             collapsed: true,
             items: [
               'ai/ai-workforce/connect-the-ai-receptionist-with-shopify',
+              'ai/ai-workforce/connect-ai-chat-receptionist-with-mls-listings',
             ],
           },
           'ai/ai-workforce/ai-voice-receptionist',
