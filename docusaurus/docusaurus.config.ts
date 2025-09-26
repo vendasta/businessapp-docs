f5668bba4c705974311ee1e55ad2971ac9abaebc
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-09-26 13:03:08 -0600

    Revert product switcher: remove Partner Center docs plugin and sidebar swizzle

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -80,19 +80,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     ],
   ],
 
-  // Additional content plugins
-  plugins: [
-    [
-      '@docusaurus/plugin-content-docs',
-      {
-        id: 'partner',
-        path: 'partner-docs',
-        routeBasePath: 'partner-center',
-        sidebarPath: './sidebars-partner.ts',
-      },
-    ],
-  ],
-
   themeConfig: {
       algolia: {
         // The application ID provided by Algolia
