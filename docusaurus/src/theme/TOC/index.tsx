d41c018e26fa5fddfbe626f6c49158de6f34c3f4
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-10-31 09:12:00 -0600

    re-styled the page actions to fit with the table of contents better

--- docusaurus/src/theme/TOC/index.tsx
+++ docusaurus/src/theme/TOC/index.tsx
@@ -2,14 +2,17 @@ import React, {type ReactElement} from 'react';
 import OriginalTOC from '@theme-original/TOC';
 import type {Props} from '@theme/TOC';
 import PageActions from '@site/src/components/PageActions';
+import styles from './styles.module.css';
 
 export default function TOCWrapper(props: Props): ReactElement {
-  const toc = <OriginalTOC {...props} />;
-  
   return (
-    <>
-      {toc}
-      <PageActions />
-    </>
+    <div className={styles.tocContainer}>
+      <div className={styles.tocContent}>
+        <OriginalTOC {...props} />
+      </div>
+      <div className={styles.pageActionsContainer}>
+        <PageActions />
+      </div>
+    </div>
   );
 }
