107e2bf49f2789138709fd67b7f26c5c20bc5caf
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-11-13 15:14:44 -0600

    Got the navbar logo working as intended

--- docusaurus/src/theme/Navbar/Logo/index.tsx
+++ docusaurus/src/theme/Navbar/Logo/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import OriginalLogo from '@theme-original/Logo';
+import React, {type ReactNode} from 'react';
+import Logo from '@theme/Logo';
 
-export default function Logo(props): JSX.Element {
-  return <OriginalLogo {...props} />;
+export default function NavbarLogo(): ReactNode {
+  return (
+    <Logo
+      className="navbar__brand"
+      imageClassName="navbar__logo"
+      titleClassName="navbar__title text--truncate"
+    />
+  );
 }
 
