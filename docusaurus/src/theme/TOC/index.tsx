d110886d95549235ab34816f2c038a419ebe78f0
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-11-13 13:03:56 -0600

    Resolve a whole whack of merge conflicts

--- docusaurus/src/theme/TOC/index.tsx
+++ docusaurus/src/theme/TOC/index.tsx
@@ -1,16 +1,20 @@
-import React, {cloneElement, type ReactElement} from 'react';
+import React, {type ReactElement} from 'react';
+import clsx from 'clsx';
 import OriginalTOC from '@theme-original/TOC';
 import type {Props} from '@theme/TOC';
 import PageActions from '@site/src/components/PageActions';
+import styles from './styles.module.css';
 
-export default function TOCWrapper(props: Props): ReactElement {
-  const toc = <OriginalTOC {...props} />;
-  return cloneElement(
-    toc as ReactElement,
-    toc.props,
-    <>
-      {toc.props.children}
-      <PageActions />
-    </>,
+export default function TOCWrapper({className, ...props}: Props): ReactElement {
+  return (
+    <div className={clsx(styles.tocContainer, className)}>
+      <OriginalTOC
+        {...props}
+        className={clsx(className, styles.tocBody)}
+      />
+      <div className={styles.pageActionsContainer}>
+        <PageActions className={styles.pageActions} />
+      </div>
+    </div>
   );
 }
