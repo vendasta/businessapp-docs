429e79abfc7dd81af9cbeb9c7e14d9b8aa78a3ce
jon-neher <62190461+jon-neher@users.noreply.github.com>
2025-11-14 09:55:41 -0600

    Add mobile-friendly product switcher to doc sidebar (#78)
    * Add mobile product switcher in docs sidebar
    
    * Made the product switcher styling better across mobile
    
    * got the dark/light toggle visually separated from the product name
    
    * Styling change to make the drop-down arrow align properly on mobile

---
+++ docusaurus/src/theme/Navbar/MobileSidebar/Header/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
+import {translate} from '@docusaurus/Translate';
+import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+import IconClose from '@theme/Icon/Close';
+import NavbarLogo from '@theme/Navbar/Logo';
+
+import styles from './styles.module.css';
+
+function CloseButton() {
+  const mobileSidebar = useNavbarMobileSidebar();
+  return (
+    <button
+      type="button"
+      aria-label={translate({
+        id: 'theme.docs.sidebar.closeSidebarButtonAriaLabel',
+        message: 'Close navigation bar',
+        description: 'The ARIA label for close button of mobile sidebar',
+      })}
+      className={clsx('clean-btn', 'navbar-sidebar__close', styles.closeButton)}
+      onClick={() => mobileSidebar.toggle()}
+    >
+      <IconClose color="var(--ifm-color-emphasis-600)" />
+    </button>
+  );
+}
+
+export default function NavbarMobileSidebarHeader() {
+  return (
+    <div className={styles.mobileHeader}>
+      <NavbarLogo />
+      <div className={styles.actions}>
+        <NavbarColorModeToggle className={styles.colorToggle} />
+        <CloseButton />
+      </div>
+    </div>
+  );
+}
